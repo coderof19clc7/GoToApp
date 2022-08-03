b3158a8c0eeea36214b74dffc7f8b06d53b2ab58
coderof19clc7 <63506231+coderof19clc7@users.noreply.github.com>
2022-08-03 17:30:50 +0700

    Update home_cubit.dart

--- front_end/GoTo/lib/views/pages/main_page/widgets/child_pages/home_page/blocs/home_cubit.dart
+++ front_end/GoTo/lib/views/pages/main_page/widgets/child_pages/home_page/blocs/home_cubit.dart
@@ -4,6 +4,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:go_to/configs/constants/dio_constants.dart';
 import 'package:go_to/configs/injection.dart';
 import 'package:go_to/cores/managers/network_manager.dart';
+import 'package:go_to/models/ors_outputs/autocomplete_output.dart';
 
 part 'home_state.dart';
 
@@ -13,10 +14,10 @@ class HomeCubit extends Cubit<HomeState> {
   final dioOpenRouteService = injector<NetworkManager>().getOpenRouteServiceDio();
 
   Future<void> getSuggestedList(int locationIndex, String inputText) async {
-    final result = await dioOpenRouteService.get(
+    final result = AutocompleteOutput.fromJson(await dioOpenRouteService.get(
       "${DioConstants.openRouteServiceBaseUrl}${DioConstants.openRouteServiceActions["autocomplete"]}?"
           "api_key=${DioConstants.openRouteServiceApiKey}&text=$inputText",
-    );
+    ));
 
     print(result);
   }
